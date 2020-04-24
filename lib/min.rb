5286db5722b7cb0ef841172e4183b80667388ea2
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-24 22:47:31 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -9,5 +9,9 @@ require 'pry'
 # }
 
 def get_the_min(groceries)
-  #code your solution here!
+  values = []
+  groceries.each do |k, v|
+    values.concat(v)
+  end
+  values.min
 end
--- lib/values.rb
+++ lib/values.rb
@@ -9,5 +9,9 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+  values = []
+  groceries.each do |k, v|
+    values.concat(v)
+  end
+  values
 end
